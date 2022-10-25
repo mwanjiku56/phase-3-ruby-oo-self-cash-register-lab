b5dcb712a6a276aa3d1653aaf7c51a017f619379
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-10-25 07:07:35 +0300

    Completed assignment

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
 
+class CashRegister
+    attr_accessor :discount, :total, :items, :price, :quantity
+    
+
+    def initialize(discount=0,total=0)
+        @discount=discount
+        @total=total
+        @items = []
+    end
+    def add_item(item,price,quantity=1)
+        @price = price
+        @quantity = quantity
+        @item = item
+        self.total += price * quantity
+        quantity.times do
+        self.items << item
+      end
+    end
+    def apply_discount
+        if @discount > 0
+            my_discount= @total*@discount/100
+            @total=@total-my_discount
+            "After the discount, the total comes to $#{@total}."
+        else 
+            "There is no discount to apply."
+        end
+
+    end
+    def void_last_transaction
+        self.total -= self.price * self.quantity
+      end
+
+
+
+end
+
